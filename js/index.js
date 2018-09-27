89731efeea1d0f6458728a79eb7fef76fe3427d4
Romanist <hello@h-w-h-p.digital>
2018-09-27 17:25:33 +0300

    ???

--- js/index.js
+++ js/index.js
@@ -1,101 +1,19 @@
-//slider-index control
+$(window).ready(function () {
+    var counter = 0;
+    $('.index-slider__prev').click(function () {
+        counter--;
+        if (counter < 0) counter = 2;
+        slideChanging(counter);
+    });
+    $('.index-slider__next').click(function () {
+        counter++;
+        if (counter >= 3) counter = 0;
+        slideChanging(counter);
+    });
 
-
-
-let indexSliderCounter = 1;
-let indexSliderCounterInner = document.querySelector('.index-slider-counter-num__is');
-let indexSliderPrev = document.querySelector('.index-slider__prev');
-let indexSliderNext = document.querySelector('.index-slider__next');
-let indexSliderFrames = document.querySelectorAll('.index-sliding-fr');
-let indexSliderArrows = document.querySelectorAll('.index-slider__arrow');
-let indexSocial = document.querySelectorAll('.social a');
-let indexSliderDashes = document.querySelectorAll('.index-slider__dash, .index-slider-counter__dash');
-let logo = document.querySelector('.logo');
-let whitePages = document.querySelectorAll('[data-theme="dark"]')
-
-let all = document.querySelectorAll('*');
-
-
-
-
-
-
-
-var iOS = navigator.userAgent.match(/iPhone|iPad|iPod/i);
-var eventFix = "click";
-
-    
-
-let indexSliderHandler = function(event) {
-    if(iOS != null) {
-        eventFix = "touchstart";
+    function slideChanging(counter) {
+        console.log(counter)
+        $('.index-sliding-fr').removeClass('opacity-true').addClass('opacity-false');
+        $('.index-sliding-fr').eq(counter).addClass('opacity-true').removeClass('opacity-false');
     }
-
-    console.log(eventFix)
-
-    indexSliderNext.removeEventListener('touchstart', indexSliderHandler);
-    indexSliderPrev.removeEventListener('touchstart', indexSliderHandler);
-    indexSliderFrames[indexSliderCounter - 1].classList.add('opacity-false');
-    indexSliderFrames[indexSliderCounter - 1].classList.remove('opacity-true');
-    if (event.target == indexSliderPrev) {
-        indexSliderCounter--;
-        if (indexSliderCounter == 0) {
-            indexSliderCounter = indexSliderFrames.length;
-        };
-    } else {
-        indexSliderCounter++;
-        if (indexSliderCounter == indexSliderFrames.length + 1) {
-            indexSliderCounter = 1;
-        };
-    }
-    for (let j = 0; j < whitePages.length; j++) {
-        if (indexSliderFrames[indexSliderCounter - 1] == whitePages[j]) {
-            for (let i = 0; i < all.length; i++) {
-                all[i].classList.add('white-theme');
-            }
-            document.querySelector('.a-tan').classList.remove('white-theme');
-            for (let i = 0; i < indexSliderArrows.length; i++) {
-                indexSliderArrows[i].classList.add('white-theme-arrows');
-            }
-            indexSocial[0].classList.add('white-theme-insta');
-            indexSocial[1].classList.add('white-theme-tel');
-            for (let i = 0; i < indexSliderDashes.length; i++) {
-                indexSliderDashes[i].classList.add('white-theme-dashes');
-            }
-            logo.classList.add('white-theme-logo');
-            j = whitePages.length;
-        } else {
-            for (let i = 0; i < all.length; i++) {
-                try {
-                    all[i].classList.remove('white-theme');
-                } catch {};
-            }
-            for (let i = 0; i < indexSliderArrows.length; i++) {
-                try {
-                    indexSliderArrows[i].classList.remove('white-theme-arrows');
-                } catch {};
-            }
-            try {
-                indexSocial[0].classList.remove('white-theme-insta');
-                indexSocial[1].classList.remove('white-theme-tel');
-                logo.classList.remove('white-theme-logo');
-            } catch {};
-            for (let i = 0; i < indexSliderDashes.length; i++) {
-                try {
-                    indexSliderDashes[i].classList.remove('white-theme-dashes');
-                } catch {};
-            }
-        }
-    }
-
-    indexSliderCounterInner.innerHTML = '0' + indexSliderCounter;
-    indexSliderFrames[indexSliderCounter - 1].classList.remove('opacity-false');
-    indexSliderFrames[indexSliderCounter - 1].classList.add('opacity-true');
-    indexSliderPrev.addEventListener('touchstart', indexSliderHandler);
-    indexSliderNext.addEventListener('touchstart', indexSliderHandler);
-}
-indexSliderPrev.addEventListener('touchstart', indexSliderHandler);
-indexSliderNext.addEventListener('touchstart', indexSliderHandler);
-
-
-
+});

cedf55de6accf25d99efff9bc6d05efdbc77545c
Alex <aleksejsch@yandex.by>
2018-09-28 12:35:06 +0300

    commit our team fix

--- js/about/about.js
+++ js/about/about.js
@@ -1,107 +1,155 @@
-let btnMassage = $('.group_write-massage').eq(0);
 
-
-
-btnMassage.on('click', (EO) => {
-	$('.about-box').hide();
-	$('.form').show();
-	$('.form').css('display', 'flex');
+//Click on Massage
+$('.write-massage').on('click', (EO) => {
+	$('.about-wrapp').addClass('animated fadeOutDown');
+	setTimeout(()=>{
+		$('.about-wrapp').hide();
+		$('.form').show();
+		$('.form').css('display', 'flex');
+	},700)
 })
 
 
 
-$('.group_send-massage').on('click', (EO) => {
-	
 
-	let name = $('.group_name input').val();
-	let tel = $('.group_tel input').val();
-	let parsTel = isNaN(tel);
-	let massage = $('.group_text input').val();
 
-	if (name === '') {
-		$('.group_name input').css('border-bottom', '1px solid #e86464');
-		$('.group_name .danger').css('display', 'block');
-		$('.group_name label').css('color', '#e86464');
-		EO.preventDefault();
+
+//======== Toogle Map-Address
+//===========================
+$('.about-mobile-link').on('click', (EO) => {
+	let target = EO.target;
+
+	if (target.tagName != 'BUTTON') {
 		return;
-	} else {
-		$('.group_name input').css('border-bottom', '1px solid #d2ab66');
-		$('.group_name .danger').css('display', 'none');
-		$('.group_name label').css('color', '#d2ab66');
 	}
 
 	
-	if (tel === '') {
-		$('.group_tel input').css('border-bottom', '1px solid #e86464');
-		$('.group_tel .danger').css('display', 'block');
-		$('.group_tel label').css('color', '#e86464');
-		EO.preventDefault();
-		return;
-	} else if(parsTel) {
-		$('.group_tel input').css('border-bottom', '1px solid #e86464');
-		$('.group_tel .danger').css('display', 'block');
-		$('.group_tel label').css('color', '#e86464');
-		EO.preventDefault();
-		return;
+
+	let adress = $('.about-tab-adress').hasClass('active');
+	let map = $('.about-tab-map').hasClass('active');	
+	let targetHasClass = $(target).hasClass('active');
+
+	if (targetHasClass) return;
+
+	if (targetHasClass === adress) {
+		console.log('+');
+		$(target).addClass('active');
+		$('.about-tab-map').removeClass('active');
 	} else {
-		$('.group_tel input').css('border-bottom', '1px solid #d2ab66');
-		$('.group_tel .danger').css('display', 'none');
-		$('.group_tel label').css('color', '#d2ab66');
+		console.log('-')
+		$(target).addClass('active');
+		$('.about-tab-adress').removeClass('active');
 	}
 
-
-
-	if (massage === '') {
-		$('.group_text input').css('border-bottom', '1px solid #e86464');
-		$('.group_text .danger').css('display', 'block');
-		$('.group_text label').css('color', '#e86464');
-		EO.preventDefault();
-		return;
+	if (!adress) {
+		$('.about-box').show();
+		$('.map').removeClass('active');
 	} else {
-		$('.group_text input').css('border-bottom', '1px solid #d2ab66');
-		$('.group_text .danger').css('display', 'none');
-		$('.group_text label').css('color', '#d2ab66');
+		$('.about-box').hide();
+		$('.map').addClass('active');
 	}
+});
 
 
+
+
+
+
+
+
+//=========Validation form
+
+$('.send-massage').click(function () {
+    var trigger = true;
+	$('.group').removeClass('warm');
+    $('.group input').each(function( index ) {
+		
+	  var _this = this;
+
+
+	  
+      if (!validate(_this, trigger)) {
+
+      	console.log(!validate(_this, trigger))
+        $(this).parent().addClass('warm');
+        trigger = false;
+	  }
+	  
+    });
+
+	if (!trigger) return false;
+	
 	$('.form').hide();
 	$('.about-thanks').show();
 	$('.about-thanks').css('display', 'flex');
+  });
 
-});
+  function validate (_this, trigger) {
+      var ck_name = /^[А-Яа-яA-Za-z\s]{1,20}$/;
+      var ck_text = /^[А-Яа-яA-Za-z0-9,.!?\s]{1,5000}$/;
+      var ck_tel = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+      var ck_number = /^\d+$/;
+      var ck_date = /^(\d{1,2}).(\d{1,2}).(\d{2}|\d{4})$/;
+      var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 
-
-
-
-
-
-$('.about-mobile-link').on('click', (EO) => {
-	let target = EO.target;
-	console.log($(target).hasClass('about-mobile-link-btn-map'))
-
-	if ($(target).hasClass('about-mobile-link-btn-map')) {
-		$('.wrapp').css('display', 'none');
-		$(target).addClass('active');
-		$('.about-mobile-link-btn-adress').removeClass('active');
-		$('.map').addClass('map-mobile-open');
-	} else {
-		$('.wrapp').css('display', 'block');
-		
-		
-		$('.map').removeClass('map-mobile-open');
-
-		
-	}
-});
-
-
-
-
-
-
-
-
-
-
+      var type = $(_this).attr('type');
+       //console.log(type)
+      if (type == 'number') {
+        if (!ck_number.test($(_this).val())){
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+      if (type == 'text') {
+        if (!ck_text.test($(_this).val())){
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+      if (type == 'password') {
+        if (!ck_text.test($(_this).val())){
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+      if (type == 'date') {
+        if (!ck_date.test($(_this).val())){
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+      if (type == 'mail') {
+        if (!ck_email.test($(_this).val())){
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+      if (type == 'tel') {
+        if (!ck_tel.test($(_this).val())){
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+      if (type == 'name') {
+        if (!ck_name.test($(_this).val())){
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+  }
 
 
